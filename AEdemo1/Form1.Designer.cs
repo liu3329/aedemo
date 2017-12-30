b279f206923b041047ae719803db22c4026c897b
liu3329 <756137342@qq.com>
2017-12-30 18:29:50 +0800

    second

--- AEdemo1/Form1.Designer.cs
+++ AEdemo1/Form1.Designer.cs
@@ -30,8 +30,13 @@ namespace AEdemo1
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
-            this.bt_openfiledialog = new System.Windows.Forms.Button();
+            this.bt_LocalMxFile = new System.Windows.Forms.Button();
             this.mainMapControl = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.bt_mapDocu = new System.Windows.Forms.Button();
+            this.bt_controlopen = new System.Windows.Forms.Button();
+            this.bt_WorkSpaceLoad = new System.Windows.Forms.Button();
+            this.bt_addshpfile = new System.Windows.Forms.Button();
+            this.bt_addRaster = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mainMapControl)).BeginInit();
             this.SuspendLayout();
@@ -45,31 +50,86 @@ namespace AEdemo1
             this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
             this.axLicenseControl1.TabIndex = 0;
             // 
-            // bt_openfiledialog
+            // bt_LocalMxFile
             // 
-            this.bt_openfiledialog.Location = new System.Drawing.Point(12, 34);
-            this.bt_openfiledialog.Name = "bt_openfiledialog";
-            this.bt_openfiledialog.Size = new System.Drawing.Size(75, 23);
-            this.bt_openfiledialog.TabIndex = 1;
-            this.bt_openfiledialog.Text = "打开地图文档";
-            this.bt_openfiledialog.UseVisualStyleBackColor = true;
-            this.bt_openfiledialog.Click += new System.EventHandler(this.bt_openfiledialog_Click);
+            this.bt_LocalMxFile.Location = new System.Drawing.Point(12, 34);
+            this.bt_LocalMxFile.Name = "bt_LocalMxFile";
+            this.bt_LocalMxFile.Size = new System.Drawing.Size(113, 23);
+            this.bt_LocalMxFile.TabIndex = 1;
+            this.bt_LocalMxFile.Text = "loadmxf打开地图文档";
+            this.bt_LocalMxFile.UseVisualStyleBackColor = true;
+            this.bt_LocalMxFile.Click += new System.EventHandler(this.bt_LocalMxFile1);
             // 
             // mainMapControl
             // 
-            this.mainMapControl.Location = new System.Drawing.Point(131, 12);
+            this.mainMapControl.Location = new System.Drawing.Point(276, 12);
             this.mainMapControl.Name = "mainMapControl";
             this.mainMapControl.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mainMapControl.OcxState")));
-            this.mainMapControl.Size = new System.Drawing.Size(798, 395);
+            this.mainMapControl.Size = new System.Drawing.Size(943, 395);
             this.mainMapControl.TabIndex = 2;
+            // 
+            // bt_mapDocu
+            // 
+            this.bt_mapDocu.Location = new System.Drawing.Point(146, 34);
+            this.bt_mapDocu.Name = "bt_mapDocu";
+            this.bt_mapDocu.Size = new System.Drawing.Size(113, 23);
+            this.bt_mapDocu.TabIndex = 3;
+            this.bt_mapDocu.Text = "mapdocu打开地图";
+            this.bt_mapDocu.UseVisualStyleBackColor = true;
+            this.bt_mapDocu.Click += new System.EventHandler(this.bt_mapDocu_Click);
+            // 
+            // bt_controlopen
+            // 
+            this.bt_controlopen.Location = new System.Drawing.Point(12, 83);
+            this.bt_controlopen.Name = "bt_controlopen";
+            this.bt_controlopen.Size = new System.Drawing.Size(113, 23);
+            this.bt_controlopen.TabIndex = 4;
+            this.bt_controlopen.Text = "controlopen打开地图";
+            this.bt_controlopen.UseVisualStyleBackColor = true;
+            this.bt_controlopen.Click += new System.EventHandler(this.bt_controlopen_Click);
+            // 
+            // bt_WorkSpaceLoad
+            // 
+            this.bt_WorkSpaceLoad.Location = new System.Drawing.Point(146, 83);
+            this.bt_WorkSpaceLoad.Name = "bt_WorkSpaceLoad";
+            this.bt_WorkSpaceLoad.Size = new System.Drawing.Size(113, 23);
+            this.bt_WorkSpaceLoad.TabIndex = 5;
+            this.bt_WorkSpaceLoad.Text = "通过工作空间加载shp";
+            this.bt_WorkSpaceLoad.UseVisualStyleBackColor = true;
+            this.bt_WorkSpaceLoad.Click += new System.EventHandler(this.bt_WorkSpaceLoad_Click);
+            // 
+            // bt_addshpfile
+            // 
+            this.bt_addshpfile.Location = new System.Drawing.Point(13, 126);
+            this.bt_addshpfile.Name = "bt_addshpfile";
+            this.bt_addshpfile.Size = new System.Drawing.Size(112, 23);
+            this.bt_addshpfile.TabIndex = 6;
+            this.bt_addshpfile.Text = "addshpfile加载";
+            this.bt_addshpfile.UseVisualStyleBackColor = true;
+            this.bt_addshpfile.Click += new System.EventHandler(this.bt_addshpfile_Click);
+            // 
+            // bt_addRaster
+            // 
+            this.bt_addRaster.Location = new System.Drawing.Point(146, 126);
+            this.bt_addRaster.Name = "bt_addRaster";
+            this.bt_addRaster.Size = new System.Drawing.Size(113, 23);
+            this.bt_addRaster.TabIndex = 7;
+            this.bt_addRaster.Text = "添加栅格图";
+            this.bt_addRaster.UseVisualStyleBackColor = true;
+            this.bt_addRaster.Click += new System.EventHandler(this.bt_addRaster_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(941, 419);
+            this.ClientSize = new System.Drawing.Size(1231, 419);
+            this.Controls.Add(this.bt_addRaster);
+            this.Controls.Add(this.bt_addshpfile);
+            this.Controls.Add(this.bt_WorkSpaceLoad);
+            this.Controls.Add(this.bt_controlopen);
+            this.Controls.Add(this.bt_mapDocu);
             this.Controls.Add(this.mainMapControl);
-            this.Controls.Add(this.bt_openfiledialog);
+            this.Controls.Add(this.bt_LocalMxFile);
             this.Controls.Add(this.axLicenseControl1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -82,8 +142,13 @@ namespace AEdemo1
         #endregion
 
         private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
-        private System.Windows.Forms.Button bt_openfiledialog;
+        private System.Windows.Forms.Button bt_LocalMxFile;
         private ESRI.ArcGIS.Controls.AxMapControl mainMapControl;
+        private System.Windows.Forms.Button bt_mapDocu;
+        private System.Windows.Forms.Button bt_controlopen;
+        private System.Windows.Forms.Button bt_WorkSpaceLoad;
+        private System.Windows.Forms.Button bt_addshpfile;
+        private System.Windows.Forms.Button bt_addRaster;
     }
 }
 
--- AEdemo1/Form1.cs
+++ AEdemo1/Form1.cs
@@ -5,7 +5,13 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+//Carto类库支持地图的创建和显示，这些地图可以在一幅地图或由许多地图及其地图元素组成的页面中包含数据。
+using ESRI.ArcGIS.Carto;
+//SystemUI类库包含用户界面组件接口定义，这些用户界面组件可以在ArcGIS Engine中进行扩展。包含ICommand、ITool和IToolControl接口。
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesFile;
+using ESRI.ArcGIS.DataSourcesRaster;
 namespace AEdemo1
 {
     public partial class Form1 : Form
@@ -15,7 +21,7 @@ namespace AEdemo1
             InitializeComponent();
         }
 
-        private void bt_openfiledialog_Click(object sender, EventArgs e)
+        private void bt_LocalMxFile1(object sender, EventArgs e)
         {
             OpenFileDialog pOpenFileDialog = new OpenFileDialog();
             pOpenFileDialog.CheckFileExists = true;
@@ -32,7 +38,7 @@ namespace AEdemo1
                 }
                 if (mainMapControl.CheckMxFile(pFileName)) //检查地图文档的有效性
                 {
-                    mainMapControl.LoadMxFile(pFileName);//地图空间加载mxd文档
+                    mainMapControl.LoadMxFile(pFileName);//地图空间加载mxd文档.loadMxfile(路径，地图名或索引，密码)
 
                 }
                 else
@@ -42,5 +48,141 @@ namespace AEdemo1
                 }
             }
         }
+
+        private void bt_mapDocu_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+            pOpenFileDialog.CheckFileExists = true;
+            pOpenFileDialog.Title = "打开地图文档";
+            pOpenFileDialog.Filter = "arcmap文档（*.mxd）|*.mxd";
+            pOpenFileDialog.Multiselect = false;
+            pOpenFileDialog.RestoreDirectory = true;
+            if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string pFileName = pOpenFileDialog.FileName;
+                if (pFileName == "")
+                {
+                    return;
+                }
+                if (mainMapControl.CheckMxFile(pFileName)) //检查地图文档的有效性
+                {
+                    //将数据载入pmapdocument并与map控件关联
+                    IMapDocument pMapDocument = new MapDocument();
+                    pMapDocument.Open(pFileName);
+                    //获取map中激活的地图文档
+                    mainMapControl.Map = pMapDocument.ActiveView.FocusMap;
+                    mainMapControl.ActiveView.Refresh();
+                   
+
+                }
+                else
+                {
+                    MessageBox.Show(pFileName + "是无效文档", "信息提示");
+                    return;
+                }
+            }
+        }
+
+        private void bt_controlopen_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bt_WorkSpaceLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+            pOpenFileDialog.CheckFileExists = true;
+            pOpenFileDialog.Title = "打开地图文档";
+            pOpenFileDialog.Filter = "shp文档（*.shp）|*.shp";
+            pOpenFileDialog.Multiselect = false;
+            pOpenFileDialog.RestoreDirectory = true;
+            pOpenFileDialog.ShowDialog();
+            string pFullPath = pOpenFileDialog.FileName;
+            //获取文件路径
+            IWorkspaceFactory pWorkspaceFactory;
+            IFeatureWorkspace pFeatureWorkspace;
+            IFeatureLayer pFeatureLayer;//提供操作图层的属性和方法
+           
+            if (pFullPath == "")
+            {
+                return;
+            }
+            else {
+                int pIndex = pFullPath.LastIndexOf("\\");//D:\aelearning\shp\，在17位
+                string pFilePath = pFullPath.Substring(0,pIndex); //0-17位是文件路径
+                string pFileName = pFullPath.Substring(pIndex+1);//18到最后是文件的名
+                //实例化shapefileworkspacefactory（）
+                pWorkspaceFactory = new ShapefileWorkspaceFactory();//创建shape文件的工作工厂
+                pFeatureWorkspace = (IFeatureWorkspace)pWorkspaceFactory.OpenFromFile(pFilePath,0);
+                //创建并实例化要素集
+                IFeatureClass pFeatureClass = pFeatureWorkspace.OpenFeatureClass(pFileName);
+                pFeatureLayer = new FeatureLayer();
+                pFeatureLayer.FeatureClass = pFeatureClass;
+                pFeatureLayer.Name = pFeatureLayer.FeatureClass.AliasName;
+                mainMapControl.Map.AddLayer(pFeatureLayer);
+                mainMapControl.ActiveView.Refresh();
+                MessageBox.Show(pIndex.ToString());
+            }
+        }
+
+        private void bt_addshpfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+            pOpenFileDialog.CheckFileExists = true;
+            pOpenFileDialog.Title = "打开地图文档";
+            pOpenFileDialog.Filter = "shp文档（*.shp）|*.shp";
+            pOpenFileDialog.Multiselect = false;
+            pOpenFileDialog.RestoreDirectory = true;
+            pOpenFileDialog.ShowDialog();
+            string pFullPath = pOpenFileDialog.FileName;
+            int pIndex = pFullPath.LastIndexOf("\\");//D:\aelearning\shp\，在17位
+            string pFilePath = pFullPath.Substring(0, pIndex); //0-17位是文件路径
+            string pFileName = pFullPath.Substring(pIndex + 1);//18到最后是文件的名
+            mainMapControl.AddShapeFile(pFilePath,pFileName);
+        }
+
+        private void bt_addRaster_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+            pOpenFileDialog.CheckFileExists = true;
+            pOpenFileDialog.Title = "打开栅格地图";
+            pOpenFileDialog.Filter = "栅格文档（*.jpg）|*.jpg";
+            pOpenFileDialog.Multiselect = false;
+            pOpenFileDialog.RestoreDirectory = true;
+            pOpenFileDialog.ShowDialog();
+            string PRasterFileName = pOpenFileDialog.FileName;
+            if (PRasterFileName == "")
+            {
+                return;
+            }
+            else {
+                string pPath = System.IO.Path.GetDirectoryName(PRasterFileName);
+                string pFileName = System.IO.Path.GetFileName(PRasterFileName);
+                IWorkspaceFactory pWorkspaceFactory = new RasterWorkspaceFactory();
+                IWorkspace pWorkspace = pWorkspaceFactory.OpenFromFile(pPath,0);
+                IRasterWorkspace pRasterWorkspace = pWorkspace as IRasterWorkspace;
+                IRasterDataset pRasterDataset = pRasterWorkspace.OpenRasterDataset(pFileName);
+                //影像金字塔判断与创建
+                IRasterPyramid3 pRasPyrmid = pRasterDataset as IRasterPyramid3;
+                if (pRasPyrmid != null)
+                {
+                    if (!(pRasPyrmid.Present)) {
+                        pRasPyrmid.Create();//创建金字塔
+                    }
+                }
+                IRaster pRaster;
+                pRaster = pRasterDataset.CreateDefaultRaster();
+                IRasterLayer pRasterLayer;
+                pRasterLayer = new RasterLayerClass();
+                pRasterLayer.CreateFromRaster(pRaster);
+                ILayer pLayer = pRasterLayer as ILayer;
+                mainMapControl.AddLayer(pLayer,0);
+
+
+
+            }
+        }
+
+
     }
 }
