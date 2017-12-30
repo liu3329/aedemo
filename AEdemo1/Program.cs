3606e618cc54d562e7e16d9bcc1411a1e46f16a9
liu3329 <756137342@qq.com>
2017-12-30 14:39:15 +0800

    first

---
+++ AEdemo1/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ESRI.ArcGIS.esriSystem;
+
+namespace AEdemo1
+{
+    static class Program
+    {
+        private static LicenseInitializer m_AOLicenseInitializer = new AEdemo1.LicenseInitializer();
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //ESRI License Initializer generated code.
+            m_AOLicenseInitializer.InitializeApplication(new esriLicenseProductCode[] { esriLicenseProductCode.esriLicenseProductCodeEngine },
+            new esriLicenseExtensionCode[] { });
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            //ESRI License Initializer generated code.
+            //Do not make any call to ArcObjects after ShutDownApplication()
+            m_AOLicenseInitializer.ShutdownApplication();
+        }
+    }
+}
