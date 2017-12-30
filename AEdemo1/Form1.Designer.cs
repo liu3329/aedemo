bae363ec4480162d00bfeb70dae2b0aa8730628c
liu3329 <756137342@qq.com>
2017-12-30 14:58:50 +0800

    loadmxd

--- AEdemo1/Form1.Designer.cs
+++ AEdemo1/Form1.Designer.cs
@@ -28,12 +28,62 @@ namespace AEdemo1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.bt_openfiledialog = new System.Windows.Forms.Button();
+            this.mainMapControl = new ESRI.ArcGIS.Controls.AxMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainMapControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(524, 314);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 0;
+            // 
+            // bt_openfiledialog
+            // 
+            this.bt_openfiledialog.Location = new System.Drawing.Point(12, 34);
+            this.bt_openfiledialog.Name = "bt_openfiledialog";
+            this.bt_openfiledialog.Size = new System.Drawing.Size(75, 23);
+            this.bt_openfiledialog.TabIndex = 1;
+            this.bt_openfiledialog.Text = "打开地图文档";
+            this.bt_openfiledialog.UseVisualStyleBackColor = true;
+            this.bt_openfiledialog.Click += new System.EventHandler(this.bt_openfiledialog_Click);
+            // 
+            // mainMapControl
+            // 
+            this.mainMapControl.Location = new System.Drawing.Point(131, 12);
+            this.mainMapControl.Name = "mainMapControl";
+            this.mainMapControl.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mainMapControl.OcxState")));
+            this.mainMapControl.Size = new System.Drawing.Size(798, 395);
+            this.mainMapControl.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 419);
+            this.Controls.Add(this.mainMapControl);
+            this.Controls.Add(this.bt_openfiledialog);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainMapControl)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private System.Windows.Forms.Button bt_openfiledialog;
+        private ESRI.ArcGIS.Controls.AxMapControl mainMapControl;
     }
 }
 
--- AEdemo1/Form1.cs
+++ AEdemo1/Form1.cs
@@ -14,5 +14,33 @@ namespace AEdemo1
         {
             InitializeComponent();
         }
+
+        private void bt_openfiledialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+            pOpenFileDialog.CheckFileExists = true;
+            pOpenFileDialog.Title = "打开地图文档";
+            pOpenFileDialog.Filter = "arcmap文档（*.mxd）|*.mxd";
+            pOpenFileDialog.Multiselect = false;
+            pOpenFileDialog.RestoreDirectory = true;
+            if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string pFileName = pOpenFileDialog.FileName;
+                if (pFileName == "")
+                {
+                    return;
+                }
+                if (mainMapControl.CheckMxFile(pFileName)) //检查地图文档的有效性
+                {
+                    mainMapControl.LoadMxFile(pFileName);//地图空间加载mxd文档
+
+                }
+                else
+                {
+                    MessageBox.Show(pFileName + "是无效文档", "信息提示");
+                    return;
+                }
+            }
+        }
     }
 }
